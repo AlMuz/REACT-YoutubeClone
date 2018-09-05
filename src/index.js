3b9fe999afcc2f2bf4ffc632fbea553f07c864e0
Aleksejs Muzičenko <aleksejs@mendo.lv>
2018-09-05 09:28:38 +0300

    add SearchBar (Lecture 14 ending)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyA5eID-0ta5A4_lNz8dm4y4C1hcp8jlQZ4';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
