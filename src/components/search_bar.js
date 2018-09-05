8fad6918d3861f57af5aa040b4bde560a6b13e15
Aleksejs Muzičenko <aleksejs@mendo.lv>
2018-09-05 10:03:09 +0300

    show state when input changes

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,20 @@
-import React  from 'react';
+import React, { Component }  from 'react';
 
-const SearchBar = () => {
-  return(
-     <input />
-  )
+class SearchBar extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render() {
+    return(
+      <div>
+        <input onChange={event => this.setState({term: event.target.value})} />
+        Value of the input: {this.state.term}
+      </div>
+    )
+  }
 }
 
 export default SearchBar;
