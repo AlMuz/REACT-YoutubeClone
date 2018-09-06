4f820594d132dd436c12fae40d54b577aa5f2191
Aleksejs Muzičenko <aleksejs@mendo.lv>
2018-09-06 09:36:48 +0300

    search functionality

--- src/index.js
+++ src/index.js
@@ -16,18 +16,22 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+    this.videoSearch('');
+  }
 
-    YTSearch({key: API_KEY, term: 'marmok'}, (videos) => {
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       })
     });
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
